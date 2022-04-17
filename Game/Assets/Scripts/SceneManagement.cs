e41c4c7f8d6f5e1253df6ca9d680aa62105032aa
DarkDragonVMC <redfirestorm07@gmail.com>
2022-04-17 19:30:23 +0200

    Menu

---
+++ Game/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,22 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void openTutorial()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void startGame()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+}
