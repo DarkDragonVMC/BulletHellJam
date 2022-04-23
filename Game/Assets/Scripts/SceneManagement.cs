54be852d7240b6925bf70bab7a3c300d2df0f893
BittninLaptop <bittninyt@gmail.com>
2022-04-23 18:09:28 +0200

    MergeFix2

--- Game/Assets/Scripts/SceneManagement.cs
+++ Game/Assets/Scripts/SceneManagement.cs
@@ -1,14 +1,47 @@
 using UnityEngine.SceneManagement;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 public class SceneManagement : MonoBehaviour
 {
+    public static bool paused;
+    public CanvasGroup pauseMenu;
+    public float fadeInSpeed;
+
+    public GameObject backToMenuButton;
+    public GameObject restartButton;
+    public GameObject quitButton;
+
+    bool fading = false;
 
     void Awake()
     {
         if (SceneManager.GetActiveScene().name == "MainMenu") SettingsManager.loadSettings(GameObject.Find("Global Volume (Effects)").GetComponent<Volume>());
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 1) return;
+
+        backToMenuButton.SetActive(false);
+        quitButton.SetActive(false);
+        restartButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                StartCoroutine(fadeOut());
+            }
+            else
+            {
+                if (FindObjectOfType<PlayerHealth>().dead) return;
+                StartCoroutine(fadeIn());
+            }
+        }
     }
 
     public void OpenSettings()
@@ -18,6 +51,7 @@ public class SceneManagement : MonoBehaviour
     public void backToMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("Wohoo");
     }
     public void openTutorial()
     {
@@ -30,5 +64,55 @@ public class SceneManagement : MonoBehaviour
     public void quit()
     {
         Application.Quit();
+    }
+
+    private IEnumerator fadeIn()
+    {
+        if (!fading)
+        {
+            fading = true;
+            backToMenuButton.SetActive(true);
+            quitButton.SetActive(true);
+            restartButton.SetActive(true);
+
+            EnemyMechanics1.saveBulletVelocity();
+            EnemyMechanics1.saveEnemyVelocity();
+
+            paused = true;
+
+
+            while (pauseMenu.alpha < 1)
+            {
+                pauseMenu.alpha += Time.deltaTime * fadeInSpeed;
+                if (pauseMenu.alpha > 1) pauseMenu.alpha = 1;
+                yield return null;
+            }
+            fading = false;
+        }
+    }
+
+    public IEnumerator fadeOut()
+    {
+        if (!fading)
+        {
+            fading = true;
+            backToMenuButton.SetActive(false);
+            quitButton.SetActive(false);
+            restartButton.SetActive(false);
+
+            EnemyMechanics1.loadBulletVelocity();
+            EnemyMechanics1.loadEnemyVelocity();
+
+            paused = false;
+
+            while (pauseMenu.alpha > 0)
+            {
+                pauseMenu.alpha -= Time.deltaTime * fadeInSpeed;
+                if (pauseMenu.alpha < 0) pauseMenu.alpha = 0;
+                yield return null;
+            }
+            fading = false;
+        }
+
     }
 }
